a1f157674d8ee090dbbdacf4801f04b627794c94
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2024-08-27 18:57:40 -0500

    Cargue de PDF

--- src/config/global.js
+++ src/config/global.js
@@ -254,7 +254,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/CF5.pdf',
       },
       //{
       //  icono: 'fas fa-download',
